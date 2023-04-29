2a89dd06ac48db49d677328780ca3fe490a025fd
Konstantinos Papadopoulos <cs04761@uoi.gr>
2023-04-29 17:26:39 +0300

    Creation of BestApplicantStrategyFactory
    Updated createStrategy method of BestApplicantStrategyFactory class
    
    Co-Authored-By: PapathanasiouElias <92224628+PapathanasiouElias@users.noreply.github.com>
    Co-Authored-By: thanasismodiotis <92262115+thanasismodiotis@users.noreply.github.com>

--- myy803_4736_4761_4765/src/main/java/diplomas_mgt_app_skeleton/model/strategies/BestApplicantStrategyFactory.java
+++ myy803_4736_4761_4765/src/main/java/diplomas_mgt_app_skeleton/model/strategies/BestApplicantStrategyFactory.java
@@ -1,4 +1,31 @@
 package diplomas_mgt_app_skeleton.model.strategies;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.lang.invoke.WrongMethodTypeException;
+
 public class BestApplicantStrategyFactory {
+    public BestApplicantStrategyFactory() {super();}
+
+    public static BestApplicantStrategy createStrategy(String name, int thresholdGrade, int thresholdCourses) throws IllegalArgumentException {
+        if (name.equals("BestAverageGrade")) {
+            BestAvgGradeStrategy strategy = new BestAvgGradeStrategy();
+            return strategy;
+        }
+        else if (name.equals("LeastRemainingCourses")){
+            FewestCoursesStrategy strategy = new FewestCoursesStrategy();
+            return strategy;
+        }
+        else if (name.equals("Threshold Strategy")){
+            ThresholdStrategy strategy = new ThresholdStrategy(thresholdGrade, thresholdCourses);
+            return strategy;
+        }
+        else if (name.equals("RandomStrategy")){
+            RandomStrategy strategy = new RandomStrategy();
+            return strategy;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid Strategy Name: " + name);
+        }
+    }
 }
