5b80f8d00723fed17922a159a83b7551199d1df0
Konstantinos Papadopoulos <cs04761@uoi.gr>
2023-05-05 20:10:20 +0300

    Update
    Changed chained if-else to switch case for better code readability
    
    Co-Authored-By: thanasismodiotis <92262115+thanasismodiotis@users.noreply.github.com>
    Co-Authored-By: PapathanasiouElias <92224628+PapathanasiouElias@users.noreply.github.com>

--- myy803_4736_4761_4765/src/main/java/diplomas_mgt_app_skeleton/model/strategies/BestApplicantStrategyFactory.java
+++ myy803_4736_4761_4765/src/main/java/diplomas_mgt_app_skeleton/model/strategies/BestApplicantStrategyFactory.java
@@ -8,24 +8,24 @@ public class BestApplicantStrategyFactory {
     public BestApplicantStrategyFactory() {super();}
 
     public static BestApplicantStrategy createStrategy(String name, int thresholdGrade, int thresholdCourses) throws IllegalArgumentException {
-        if (name.equals("BestAverageGrade")) {
-            BestAvgGradeStrategy strategy = new BestAvgGradeStrategy();
-            return strategy;
-        }
-        else if (name.equals("LeastRemainingCourses")){
-            FewestCoursesStrategy strategy = new FewestCoursesStrategy();
-            return strategy;
-        }
-        else if (name.equals("Threshold Strategy")){
-            ThresholdStrategy strategy = new ThresholdStrategy(thresholdGrade, thresholdCourses);
-            return strategy;
-        }
-        else if (name.equals("RandomStrategy")){
-            RandomStrategy strategy = new RandomStrategy();
-            return strategy;
-        }
-        else {
-            throw new IllegalArgumentException("Invalid Strategy Name: " + name);
+        switch (name) {
+            case "BestAverageGrade" -> {
+                BestAvgGradeStrategy strategy = new BestAvgGradeStrategy();
+                return strategy;
+            }
+            case "LeastRemainingCourses" -> {
+                FewestCoursesStrategy strategy = new FewestCoursesStrategy();
+                return strategy;
+            }
+            case "Threshold Strategy" -> {
+                ThresholdStrategy strategy = new ThresholdStrategy(thresholdGrade, thresholdCourses);
+                return strategy;
+            }
+            case "RandomStrategy" -> {
+                RandomStrategy strategy = new RandomStrategy();
+                return strategy;
+            }
+            default -> throw new IllegalArgumentException("Invalid Strategy Name: " + name);
         }
     }
 }
